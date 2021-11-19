b2789840754249bb5d0ca11d1d13d30c080f36c0
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2021-11-20 00:20:26 +0530

    first and last position of element in sorted array

---
+++ leetcode/Medium/first_last_pos_of_element.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans(2);
+        ans[0]=-1;
+        ans[1]= -1;
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
+                ans[0] = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        low = 0;
+        high = nums.size() - 1;
+          while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
+                ans[1] = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
