7fd276e0e566a054e9d6ac25a9c15fbaf2901767
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-01-25 00:34:35 +0530

    shortest distance to a character

---
+++ leetcode/Easy/shortest_distance_to_a_character.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        int i = 0;
+        int n = s.length();
+        vector<int> ans(n);
+        int l = 0;
+        int r = 0;
+        while(i < n){
+            if(s[i] == c){
+                l = i;
+                r = i + 1;
+            }
+            while(r < n && s[r] != c) r++;
+            while(l < n && s[l] != c) l++;
+            if(r == n) r = INT_MAX;
+            ans[i] = min(abs(l-i), abs(r-i));
+            i++;
+        }
+        return ans;
+    }
+};
