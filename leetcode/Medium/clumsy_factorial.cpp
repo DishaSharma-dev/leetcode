151b2566a69dae950d39c125d80fe26a72461ace
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-02-24 20:31:15 +0530

    clumsy factorial

---
+++ leetcode/Medium/clumsy_factorial.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int clumsy(int n) {
+        int ans = 1;
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        if(n == 3){
+            return 6;
+        }
+        if(n == 4){
+            return 7;
+        }
+        if( n >= 5){
+            if(n % 4 == 0) return n+1;
+            else if(n % 4 == 1) return n+2;
+            else if(n % 4 == 2) return n+2;
+            else if(n % 4 == 3) return n-1;
+        }
+        return ans;
+    }
+};
