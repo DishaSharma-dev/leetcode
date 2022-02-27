afa73064a30130692d6d61c3286239859ec86347
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-02-27 23:07:25 +0530

    minimum time to complete trips

---
+++ leetcode/Medium/minimum_time_to_complete_trips.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) {
+       long long l = 0, r = 100000000000000, ans = r;
+        while(l < r){
+            long long mid = (l + r)/2;
+            long long count = 0;
+            for(int i = 0; i < time.size(); i++){
+                count +=(mid/time[i]);
+            }
+            if(count >= totalTrips){
+                ans = min(ans, mid);
+                r = mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+};
