3952922e15476ec2f8784ef52b4e47c12bdcd1d9
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-03-17 23:49:10 +0530

    score parenthesis

---
+++ leetcode/Medium/score_parenthesis.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+       int ans = 0;
+       int open = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
+                open++;
+            }
+            else{
+                open--;
+                if(s[i-1] == '('){
+                    ans += pow(2, open);
+                }
+            }
+        }
+        return ans;
+    }
+};
