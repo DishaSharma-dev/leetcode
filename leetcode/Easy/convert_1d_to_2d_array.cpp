97fe79a62fdcb18f12676a84486256cd0ec50167
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-06-06 00:13:57 +0530

    convert 1d to 2d array

---
+++ leetcode/Easy/convert_1d_to_2d_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> result;
+        if(original.size() > m *n || original.size() < m*n) return result;
+        int count = 0;
+        for(int i = 0; i < m; i++){
+            vector<int> temp;
+            for(int j = 0; j < n; j++){
+                temp.push_back(original[count++]);
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
